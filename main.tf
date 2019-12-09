1a37dd77bb89684e223a757560623d85940b1bb4
Simon Elliott <simonelliott@simons-mbp.home>
2019-12-09 17:36:52 +0000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "google_compute_network" "network" {
+  name                                  = "${var.project}-vpc"
+  routing_mode                          = "${var.routing_mode}"
+  auto_create_subnetworks               = "${var.auto-subnets}"
+  description                           = "${var.description}"
+
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  count                                 = "${var.subnet_count}"
+  name                                  = "${var.project}-${var.name}-subnet-${count.index +1}"
+  ip_cidr_range                         = "${element(var.subnetwork_ip_range,count.index)}"
+  region                                = "${var.region}"
+  network                               = "${google_compute_network.network.name}"
+  private_ip_google_access              = true
+
+}
+
+
+resource "google_compute_router" "router" {
+  name                                  = "${var.project}-router"
+  region                                = "${var.region}"
+  network                               = "${google_compute_network.network.self_link}"
+  bgp {
+    asn = 64514
+}
+}
+
+resource "google_compute_address" "address" {
+  count                                 = "${var.subnet_count}"
+  name                                  = "${var.project}-${var.name}-nat-ip-${count.index +1}"
+  region                                = "${var.region}"
+}
+
+resource "google_compute_router_nat" "advanced-nat" {
+  count                                 = "${var.subnet_count}"                        
+  name                                  = "${var.project}-nat-${count.index +1}"
+  router                                = "${google_compute_router.router.name}"
+  region                                = "${var.region}"
+  nat_ip_allocate_option                = "MANUAL_ONLY"
+  nat_ips                               = ["${google_compute_address.address.*.self_link[count.index]}"]
+  source_subnetwork_ip_ranges_to_nat    = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                                = "${google_compute_subnetwork.subnet.*.self_link[count.index]}"
+    source_ip_ranges_to_nat             = ["PRIMARY_IP_RANGE"]
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable env                {}
+variable name               {}
+variable ipv4_range         {}
+variable subnetwork_ip_range    {type="list"}
+variable auto-subnets       {}
+variable routing_mode       {}
+variable subnet_count       {}
+variable description        {}
+variable region             {}
