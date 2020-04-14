a2a9f1fec158f2e98058eaae35c66abb04dde27a
Simon Elliott <simoncelliott@gmail.com>
2020-04-14 10:52:13 +0100

    added vpc_name output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = google_compute_network.network.id
 }
 
+output "vpc_name" {
+  value = google_compute_network.network.name
+}
+
 output "vpc_gateway" {
   value = google_compute_network.network.gateway_ipv4
 }
